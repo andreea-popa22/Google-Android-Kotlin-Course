745ff66111db19638773c8f97dddb4ef732c36e6
Andreea <andreea_popa22@yahoo.ro>
2022-04-24 23:43:10 +0300

    add dice images

--- MyApp/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,24 +3,42 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-
-    private var diceRollCounter: Int = 0
-    private lateinit var rollDiceNumber: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rollDiceNumber = findViewById(R.id.textView)
-        rollDiceNumber.text = diceRollCounter.toString()
-        val rollDice: Button = findViewById(R.id.button)
-        rollDice.setOnClickListener {
-            diceRollCounter++
-            rollDiceNumber.text = diceRollCounter.toString()
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
