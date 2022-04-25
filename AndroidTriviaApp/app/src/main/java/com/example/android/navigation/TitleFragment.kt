96f0c7443ed068d05f947686bd3b81da49413743
Andreea <andreea_popa22@yahoo.ro>
2022-04-25 22:55:27 +0300

    add android trivia app

---
+++ AndroidTriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,21 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class TitleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+        return binding.root
+    }
+}
